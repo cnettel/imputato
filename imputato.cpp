2434ca9f0ad6f85e8daeb0d12c6ea3b416533db9
cnettel <carl.nettelblad@it.uu.se>
2024-03-15 16:22:46 +0100

    Stub more

--- imputato.cpp
+++ imputato.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <numeric>
 #include <array>
 #include <eigen3/Eigen/Dense>
 #include <vector>
@@ -16,10 +18,7 @@ struct map
 
 template<class column> void doemit(column& c, genprob& prior, int marker);
 
-template<class column> void dotransition(column& c, column& d, const map& themap, int marker, int d)
-{
-
-}
+template<class column> void dotransition(column& c, column& d, const map& themap, int marker, int d);
 
 struct haplotype
 {
@@ -30,10 +29,32 @@ struct haplotype
     MatrixXf fwbw[2];
 };
 
-template<class column> void doemit(column& c, genprob& prior, int marker);
-{
+vector<haplotype> haplotypes;
 
-};
+template<class column> void doemit(column& c, genprob& prior, int marker)
+{
+    for (int i = 0; i < haplotypes.size(); i++)
+    {
+        float val = 0.0f;
+        for (int j = 0; j < 2; j++)
+        {
+            val += prior[j] * haplotypes[i][j];
+        }
+    }
+}
+
+template<class column> void dotransition(column& c, column& c2, const map& themap, int marker, int d)
+{
+    float dist = (map.chromposes[marker + d] - map.chromposes[marker]) * d;
+    float nonrec = expf(dist * -0.02 * Ne);
+    float rec = (1.0f - nonrec) / haplotypes.size();
+    float sum = std::reduce(c.begin(), c.end());
+
+    for (int i = 0; i < haplotypes.size(); i++)
+    {
+        c2[i] = c[i] 
+    }
+}
 
 struct individ
 {
